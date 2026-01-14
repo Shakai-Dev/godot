10b8b8ef8a5447653fd82200daeae44302329697
AR <ardev1.deverson@proton.me>
2026-01-14 17:21:14 +0500

    Possible work around for errors

--- scene/resources/camera_attributes.cpp
+++ scene/resources/camera_attributes.cpp
@@ -125,16 +125,18 @@ int CameraAttributes::get_motion_blur_sample_count() const {
 	return motion_blur_sample_count;
 }
 
-void CameraAttributes::set_motion_blur_quality(CameraAttributes::MotionBlurQuality p_quality) {
-	if (motion_blur_quality == p_quality) {
+void CameraAttributes::set_motion_blur_quality(int p_quality) {
+	p_quality = CLAMP(p_quality, MOTION_BLUR_QUALITY_LOW, MOTION_BLUR_QUALITY_HIGH);
+	MotionBlurQuality quality = (MotionBlurQuality)p_quality;
+	if (motion_blur_quality == quality) {
 		return;
 	}
-	motion_blur_quality = p_quality;
+	motion_blur_quality = quality;
 	emit_changed();
 }
 
-CameraAttributes::MotionBlurQuality CameraAttributes::get_motion_blur_quality() const {
-	return motion_blur_quality;
+int CameraAttributes::get_motion_blur_quality() const {
+	return int(motion_blur_quality);
 }
 
 RID CameraAttributes::get_rid() const {
